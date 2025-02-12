f18486051e2133a8cd15eb2ce06bad7430be9edc
luffy977338 <timur.djeletyan.2009@gmail.com>
2025-02-12 20:32:30 +0300

    set up

---
+++ packages/database/src/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(databaseUrl?: string) {
+    super({
+      datasources: {
+        db: {
+          url: databaseUrl,
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
